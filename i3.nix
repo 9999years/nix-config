1e4b06d3c61694433856d414662f971df9eea83c
Rebecca Turner <rbt@sent.as>
2020-01-11 15:54:36 -0500

    Wallpaper setup

--- i3.nix
+++ i3.nix
@@ -3,35 +3,40 @@
   services.xserver = {
     enable = lib.mkDefault true;
     dpi = lib.mkDefault 175;
+
     libinput = {
       enable = true;
       naturalScrolling = true; # Windows-style, baby!
     };
 
-    desktopManager = {
+    displayManager = {
+      lightdm.background = "/etc/nixos/background-image.jpg";
+    };
+
+    desktopManager = rec {
       default = "xfce";
-      wallpaper.mode = "fill";
+      wallpaper = {
+        mode = "fill";
+        combineScreens = false;
+      };
+      xterm.enable = false;
       xfce = {
         enable = true;
         noDesktop = true;
         enableXfwm = false;
+        # NixOS incorrectly guesses that xfce (or maybe xfwm...?) can handle setting
+        # the background. It cannot -- at least not when we're using it like this,
+        # so we have to patch the script back in. It just uses feh to set the background.
+        # https://github.com/NixOS/nixpkgs/blob/ae1cb0bdf98a325062eb9c5bd6af350ca9cf6bf2/nixos/modules/services/x11/desktop-managers/default.nix#L76-L81
+        extraSessionCommands = ''
+          if [ -e $HOME/.background-image ]; then
+            ${pkgs.feh}/bin/feh --bg-${wallpaper.mode} \
+                                ${lib.optionalString wallpaper.combineScreens "--no-xinerama"} \
+                                $HOME/.background-image
+          fi
+        '';
       };
-      xterm.enable = false;
     };
-
-    # NixOS incorrectly guesses that xfce (or maybe xfwm...?) can handle setting
-    # the background. It cannot -- at least not when we're using it like this,
-    # so we have to patch the script back in. It just uses feh to set the background.
-    # https://github.com/NixOS/nixpkgs/blob/ae1cb0bdf98a325062eb9c5bd6af350ca9cf6bf2/nixos/modules/services/x11/desktop-managers/default.nix#L76-L81
-    services.xserver.desktopManager.xfce.extraSessionCommands =
-      let cfg = config.services.xserver;
-      in ''
-            if [ -e $HOME/.background-image ]; then
-              ${pkgs.feh}/bin/feh --bg-${cfg.wallpaper.mode} \
-                                  ${lib.optionalString cfg.wallpaper.combineScreens "--no-xinerama"} \
-                                  $HOME/.background-image
-            fi
-      '';
 
     windowManager.i3 = {
       package = pkgs.i3-gaps;

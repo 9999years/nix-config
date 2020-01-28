135e7769bcd7278dbd8930cfd81daad31ecfbc56
Rebecca Turner <rbt@sent.as>
2020-01-28 10:06:00 -0500

    add psftools

---
+++ pkg/psftools/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs ? import <nixpkgs> {},
+}:
+with pkgs; stdenv.mkDerivation rec {
+  name = "psftools";
+  version = "0.5.1";
+
+  src = fetchurl {
+    url = "https://tset.de/downloads/psftools-0.5.1.tar.gz";
+    sha512 =
+      "32v3w3kcb5gwsaividljn0j11jgfxmz4igpywz23z2q1iqjdx041dh38xm3am50fxahxkc8wsa791i2fmi919rdywhaslizav8w04p2";
+  };
+
+  preInstall = ''
+      BINDIR="$out/bin"
+      CONSOLEFONTDIR="$out/share/consolefonts"
+      mkdir -p $BINDIR $CONSOLEFONTDIR
+      export MAKEFLAGS="BINDIR=$BINDIR CONSOLEFONTDIR=$CONSOLEFONTDIR"
+    '';
+}

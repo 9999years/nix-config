1245667b9db8604185313e8dc24e45c3d69b842a
Rebecca Turner <rbt@sent.as>
2020-08-27 09:41:10 -0400

    update syncthing settings

--- syncthing.nix
+++ syncthing.nix
@@ -3,24 +3,25 @@
     enable = true;
     user = "becca";
     configDir = "/home/${user}/.config/syncthing";
-    dataDir = "/home/${user}";
-    declarative = {
+    dataDir = "/home/${user}/Documents";
+    declarative = rec {
       devices = {
-        cervina.id =
-          "CJBBCGF-MWA6CVR-NPR5C57-7RXNFGR-ITIBB7S-WS25XW7-HOXVPOO-SBNKSAT";
-        aquatica.id =
-          "OQ3QP4K-BP77TZU-GLG2BAV-I4TZ36S-C75DQYY-PQTKHOI-VX7S5L4-B4R7UQQ";
+        cervina-windows.id =
+          "76RKFLL-KE56JKM-DA4VUWT-TTDF76A-RSTZ5GR-XLMCJMY-EQR7J6J-G54V7AG";
+        aquatica-nixos.id =
+          "TA5A7NS-UVQZMCT-7KHNRQQ-N2CRLYT-NAXP77O-R4WGJEK-RZV3OZX-D265ZQ3";
       };
-      folders = {
+      folders = let allDevices = lib.attrNames devices;
+      in {
         "${dataDir}/pdf" = {
           id = "pdf";
           label = "pdf";
-          devices = [ "cervina" "aquatica" ];
+          devices = allDevices;
         };
         "${dataDir}/Fonts" = {
           id = "fonts";
           label = "fonts";
-          devices = [ "cervina" "aquatica" ];
+          devices = allDevices;
         };
       };
     };

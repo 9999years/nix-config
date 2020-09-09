96329bf3a01a6cb01c7975744e338204f1a5ab17
Rebecca Turner <rbt@sent.as>
2020-09-09 10:06:00 -0400

    update spdx-tool

--- rebeccapkgs/spdx-tool/default.nix
+++ rebeccapkgs/spdx-tool/default.nix
@@ -2,6 +2,6 @@
 import (fetchFromGitHub {
   owner = "9999years";
   repo = "spdx-tool";
-  rev = "v1.0.2";
-  sha256 = "16fwvzpqa3g62f0ba4lb181jhazj2svkwnf7khp33x1wpvc9kd8c";
+  rev = "v1.0.3";
+  sha256 = "1ybrmkamk6ii85j4dhs741pi5vc45flbk05c1w7yb6hpx22gqggp";
 }) args

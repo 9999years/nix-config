04cb10b2a7b99e53620c793e9624e810ca486013
Rebecca Turner <rbt@sent.as>
2020-09-09 11:57:38 -0400

    spdx 1.0.3->1.0.4

--- rebeccapkgs/spdx-tool/default.nix
+++ rebeccapkgs/spdx-tool/default.nix
@@ -1,7 +1,7 @@
-{ stdenv, lib, fetchFromGitHub, python38 }@args:
+{ stdenv, lib, fetchFromGitHub, python38, fzf }@args:
 import (fetchFromGitHub {
   owner = "9999years";
   repo = "spdx-tool";
-  rev = "v1.0.3";
-  sha256 = "1ybrmkamk6ii85j4dhs741pi5vc45flbk05c1w7yb6hpx22gqggp";
+  rev = "v1.0.4";
+  sha256 = "12qw5rq7zx2w03pj1n4axlg3gc6ywyyjm1slxrl7ivw387dn76m4";
 }) args

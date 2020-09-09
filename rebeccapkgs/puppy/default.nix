7207bcf505137f3c56a891870ee8b7f99ac4802e
Rebecca Turner <rbt@sent.as>
2020-09-09 10:15:12 -0400

    update puppy 0.0.3 -> 0.0.5

--- rebeccapkgs/puppy/default.nix
+++ rebeccapkgs/puppy/default.nix
@@ -1,11 +1,11 @@
 { rustPlatform, fetchCrate }:
 rustPlatform.buildRustPackage rec {
   pname = "puppy";
-  version = "0.0.3";
+  version = "0.0.5";
   src = fetchCrate {
     inherit version;
     crateName = pname;
-    sha256 = "13py8mc10ggzn6y55vrfc31fq9kw1xzsqa9757cbvjxnrm257gh0";
+    sha256 = "0kxkp40386c13f461mdlm0kkdqnbnw0ik28yww5m8k2axry1lfix";
   };
-  cargoSha256 = "13zj0d0ssf7w9d7vxnrnsmvy6v3i4w1rh1wizjyrsnmhqf8596bv";
+  cargoSha256 = "05zgappvkflskq02l2w8wncrvj7r8bq0nmp7yl9l0dcrigw29s5b";
 }

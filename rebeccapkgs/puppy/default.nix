10700757f6326e4473388939fa9a3ac242fd80d2
Rebecca Turner <rbt@sent.as>
2020-10-17 20:17:15 -0400

    puppy 0.0.7 -> 0.0.8

--- rebeccapkgs/puppy/default.nix
+++ rebeccapkgs/puppy/default.nix
@@ -1,11 +1,11 @@
 { rustPlatform, fetchCrate }:
 rustPlatform.buildRustPackage rec {
   pname = "puppy";
-  version = "0.0.7";
+  version = "0.0.8";
   src = fetchCrate {
     inherit version;
     crateName = pname;
-    sha256 = "0zxksg3ywnddm5i7ipcp2v103j64yrh3vd0z9isxv9caa6adlb1b";
+    sha256 = "0dbr5phscr51l7appgma4s622s944fbmc3zsb21cn92rj015k2ii";
   };
-  cargoSha256 = "1qr6ng9hh2jz5qmqcp2l002djq9kqxxr42wsl827pj3vpgx3bp92";
+  cargoSha256 = "1x9wzlykxl5lzvpsfg92ix9lw6spgzk3r30my3hlc4060vwnj8m3";
 }

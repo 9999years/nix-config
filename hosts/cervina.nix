466fdbe1aea6623714eaff7e670705d36f1ceb60
Rebecca Turner <rbt@sent.as>
2020-10-31 20:44:57 -0400

    hosts

--- hosts/cervina.nix
+++ hosts/cervina.nix
@@ -4,25 +4,37 @@
   networking.hostName = "cervina";
 
   # Why is the WiFi card called that!!
-  networking.interfaces.wlp59s0.useDHCP = true;
+  networking = {
+    useDHCP = false;
+    interfaces.wlp59s0.useDHCP = true;
+  };
 
-  # Do NOT keep /tmp in RAM because /tmp can get big and this is but a laptop.
-  boot.tmpOnTmpfs = false;
+  boot = {
+    # Do NOT keep /tmp in RAM because /tmp can get big and this is but a laptop.
+    tmpOnTmpfs = false;
+    kernelModules = [ "coretemp" ];
+    loader.grub.fontSize = 32;
+  };
 
-  hardware.cpu.intel.updateMicrocode = true;
-  hardware.enableAllFirmware = true;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableAllFirmware = true;
+
+    bumblebee.enable = true;
+    pulseaudio.enable = true;
+  };
 
   # Save our fans / CPU temps.
-  boot.kernelModules = [ "coretemp" ];
   powerManagement = {
     enable = true;
+    cpuFreqGovernor = "performance";
     powertop.enable = true;
   };
 
-  boot.loader.grub.fontSize = 32;
-
-  console.packages = with pkgs; [ terminus_font ];
-  console.font = "ter-c32n";
+  console = {
+    packages = with pkgs; [ terminus_font ];
+    font = "ter-c32n";
+  };
 
   services.xserver.dpi = 216;
 
@@ -30,7 +42,4 @@
     [
       light # backlight control
     ];
-
-  hardware.bumblebee.enable = true;
-  hardware.pulseaudio.enable = true;
 }

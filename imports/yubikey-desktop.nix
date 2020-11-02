a15dcda76c3a3a39528fe5524f4437acaf13acf2
Rebecca Turner <rbt@sent.as>
2020-11-02 18:34:30 -0500

    u2f: fix on desktop...?

--- imports/yubikey-desktop.nix
+++ imports/yubikey-desktop.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, lib, ... }: {
   # Yubikey support
   services.pcscd.enable = lib.mkDefault true;
   services.udev.packages = with pkgs;
     lib.mkDefault [ libu2f-host yubikey-personalization pam_u2f ];
   programs.ssh.startAgent = true;
+
+  security.pam = {
+    enableSSHAgentAuth = true;
+    services.sudo.sshAgentAuth = true;
+  };
 }

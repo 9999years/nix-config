007fae0c63d1c8223f820d9bcd326dccbc2276c3
Rebecca Turner <rbt@sent.as>
2020-11-02 17:32:27 -0500

    yubikey: -gnupg

--- imports/yubikey.nix
+++ imports/yubikey.nix
@@ -13,18 +13,7 @@ in {
   services.pcscd.enable = lib.mkDefault true;
   services.udev.packages = with pkgs;
     lib.mkDefault [ libu2f-host yubikey-personalization pam_u2f ];
-  environment.shellInit = lib.mkDefault ''
-    export GPG_TTY=$(tty)
-    gpg-connect-agent /bye
-    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
-  '';
-  programs = {
-    ssh.startAgent = lib.mkDefault false;
-    gnupg.agent = {
-      enable = lib.mkDefault true;
-      enableSSHSupport = true;
-    };
-  };
+  programs.ssh.startAgent = true;
 
   security.pam = {
     enableSSHAgentAuth = true;

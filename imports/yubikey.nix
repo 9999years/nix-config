ae9af66d46b3c7da227f4d8a54d79e719ed95e85
Rebecca Turner <rbt@sent.as>
2020-11-02 15:53:57 -0500

    server: add yubikey-over-ssh-for-sudo support
    look, i have no clue if the public keys that pamu2fcfg prints are
    sensitive. if they are, you have to tell me so i can redact this.
    shame the docs aren't clearer...

--- imports/yubikey.nix
+++ imports/yubikey.nix
@@ -1,8 +1,18 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  inherit (pkgs) writeTextFile;
+  inherit (lib) concatStringsSep mapAttrsToList;
+  keys = import ../resources/yubikeys.nix;
+  u2fAuth = keyAttrs:
+    concatStringsSep "\n"
+    (mapAttrsToList (user: keys: user + ":" + (concatStringsSep ":" keys))
+      keyAttrs);
+  u2fAuthFile = keyAttrs: writeTextFile "u2f_mappings" (u2fAuth keyAttrs);
+in {
   # Yubikey support
   services.pcscd.enable = lib.mkDefault true;
   services.udev.packages = with pkgs;
-    lib.mkDefault [ libu2f-host yubikey-personalization ];
+    lib.mkDefault [ libu2f-host yubikey-personalization pam_u2f ];
   environment.shellInit = lib.mkDefault ''
     export GPG_TTY=$(tty)
     gpg-connect-agent /bye
@@ -13,6 +23,21 @@
     gnupg.agent = {
       enable = lib.mkDefault true;
       enableSSHSupport = true;
+    };
+  };
+
+  security.pam = {
+    enableSSHAgentAuth = true;
+    services.sudo.sshAgentAuth = true;
+    u2f = {
+      enable = true;
+      appId = "pam://becca.ooo";
+      cue = true;
+      authFile = with keys;
+        u2fAuthFile {
+          root = [ mobile cervina aquatica ];
+          becca = [ mobile cervina aquatica ];
+        };
     };
   };
 }
---
+++ resources/yubikeys.nix
@@ -0,0 +1,9 @@
+# Generated with pamu2fcfg
+{
+  mobile =
+    "jRGNOzBQQHNQNuv6ialOkawsSgVnB019ZpI1v/UnyN3O5ImXN+bRLlLPYzWIznvfWiX3H3QhL2ORFkEyLfe49g==,vrys5h3lOQtpY277A5jmnQ0Tn/+kbpOm/gG9DGTpypB4N3po894t0sGyAFLRD34XbWsIAC0I5Oy4DovBJt6QMw==,es256,+presence";
+  cervina =
+    "NvAskpNC89sJ+CRDZHNGp2X+4RPffmGaHeZuJasgux0ygJOfkrU4tz/tnftXwPOtQk2u9j8Cu4BNK/TH0fD4/w==,iVCQmYr7uZunygfRXGQC3TzFhNEDA8FHsSD6N9egwVW/ewh9gzNw0HZynr7Y/yG/MZO0yVePm2Q6vyF+zomngw==,es256,+presence";
+  aquatica =
+    "rfnhteEYJoBVMJ0auWfGdPdd6TSNKVdPrPZF0/oj1UZqM7t297iRNyJTKCIlREXc3aG1BTiXWazFT2NzqHdQkA==,PJvboPKj2Ir1viPA3we3zc1XTFlHRxGO4TTDRXxtMPgsOxvf5sxbCE4orO66yoe3jrpXK5bexCpoH7SMPHcTiw==,es256,+presence";
+}

9b505910e2d08cbf3451501cbfe1c8c74e159020
Rebecca Turner <rbt@sent.as>
2020-11-02 19:23:14 -0500

    enable yubico auth...?

--- imports/yubikey-server.nix
+++ imports/yubikey-server.nix
@@ -13,11 +13,20 @@ in {
     enableSSHAgentAuth = true;
     services.sudo = {
       u2fAuth = true;
+      yubicoAuth = true;
       sshAgentAuth = true;
     };
-    u2f = {
+
+    yubico = {
       enable = true;
       debug = true;
+      # 16 is just a magic number:
+      #    https://developers.yubico.com/yubico-pam/YubiKey_and_SSH_via_PAM.html
+      id = "16";
+    };
+
+    u2f = {
+      enable = true;
       appId = "pam://becca.ooo";
       cue = true;
       authFile = with keys.${config.networking.hostName};
